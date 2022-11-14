3c4b865385b201257d02ac328cbff027af063a2d
Lucas Prett Campagna <lucaspcampagna@gmail.com>
2022-11-14 18:36:08 -0300

    api first commit

--- api/main.py
+++ api/main.py
@@ -1,9 +1,5 @@
-import json
-import requests
-
-from flask import Flask, request, render_template, redirect, flash, url_for
-from flask import session, make_response, g
-from flask import current_app
+from flask import Flask, request, jsonify
+from flask import g
 
 from flask import Flask
 
@@ -13,16 +9,22 @@ from utils.auth import get_admin, create_user, get_oidc, get_token, check_token
 app = Flask(__name__)
 
 app.config.from_object('utils.settings')
-app.config.from_envvar('KEYCLOAK_FLASK_SETTINGS')
+# app.config.from_envvar('KEYCLOAK_FLASK_SETTINGS')
 
-g.username = ''
-g.access_token = ''
-
-@app.route('/',methods=['GET','POST'])
+@app.route('/',methods=['POST'])
 def home():
-    if g.username is None or g.access_token is None:
-        return redirect(url_for('login'))
-    return '123123' 
+    if request.headers.get("content-type") == 'application/json':
+        if not 'username' in request.json.keys():
+            return jsonify(dict(status=-1,message='username required'))
+        if not 'password' in request.json.keys():
+            return jsonify(dict(status=-1,message='password required'))
+        g.username = request.json.get('username')
+        g.password = request.json.get('password')
+        oidc_obj = get_oidc()
+        token = get_token(oidc_obj, g.username, g.password)
+        print("\nTOKEN: %s\n" % token)
+        return jsonify(dict(status=1,message=token))
+    return jsonify(dict(status=-1,message=''))
 
 @app.route('/login',methods=['GET','POST'])
 def login():
--- api/utils/settings.py
+++ api/utils/settings.py
@@ -6,7 +6,7 @@ SERVER_URL = "localhost:80"
 ADMIN_USERNAME = "user"
 ADMIN_PASS = "bitnami"
 REALM_NAME = "Carford"
-CLIENT_ID = "control"
+CLIENT_ID = "Control"
 CLIENT_SECRET = ""
 
 INGRESS_HOST = ""
